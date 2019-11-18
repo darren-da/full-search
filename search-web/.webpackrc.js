a1ceaec318bd025c978131204886c69d6df015fa
83908472@qq.com <83908472@qq.com>
2019-11-18 22:05:25 +0800

    全文检索

---
+++ search-web/.webpackrc.js
@@ -0,0 +1,17 @@
+import { resolve } from 'path';
+
+export default {
+  theme: './src/theme.js',
+  alias: {
+    components: resolve(__dirname, 'src/components'),
+    services: resolve(__dirname, 'src/services'),
+    assets: resolve(__dirname, 'src/assets'),
+  },
+  "proxy": {
+    "/api": {
+      "target": "http://localhost:8080/api",
+      "changeOrigin": true,
+      "pathRewrite": { "^/api" : "" }
+    }
+  }
+};
